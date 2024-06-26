a3db3a28b50ccebf7e4365267d04b5f43e7d0d2e
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-06-26 19:36:11 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -9,6 +9,11 @@ exports('GetCoreObject', function()
     return QBCore
 end)
 
+RegisterNetEvent('refine-ambulance:fullhealth', function() --refine-ambulance
+    TriggerEvent('refine-hp:client:Revive')
+    TriggerEvent('refine-hp:client:adminHeal')
+end)
+
 -- To use this export in a script instead of manifest method
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qb-core']:GetCoreObject()
