4066a32e96091eda1ffd9936b389d06999cebf76
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-06-26 19:35:52 -0400

    Update jobs.lua

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -225,6 +225,19 @@ QBShared.Jobs = {
 			['4'] = { name = 'Manager', isboss = true, payment = 150 },
 		},
 	},
+	mosleys = {
+		label = 'Mosleys',
+		type = 'mechanic',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+			['0'] = { name = 'Recruit', payment = 50 },
+			['1'] = { name = 'Novice', payment = 75 },
+			['2'] = { name = 'Experienced', payment = 100 },
+			['3'] = { name = 'Advanced', payment = 125 },
+			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+		},
+	},
     lscustoms68 = {
 		label = 'LS Customs Route 68',
 		type = 'mechanic',
