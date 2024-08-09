0afb93df513b46062de85df18353235bd2cd8571
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-08-09 15:46:24 -0400

    Update jobs.lua

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -255,6 +255,19 @@ QBShared.Jobs = {
 			['4'] = { name = 'Manager', isboss = true, payment = 150 },
 		},
 	},
+	bigtuna = {
+		label = 'Big Tuna',
+		type = 'mechanic',
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+			['0'] = { name = 'Recruit', payment = 50 },
+			['1'] = { name = 'Novice', payment = 75 },
+			['2'] = { name = 'Experienced', payment = 100 },
+			['3'] = { name = 'Advanced', payment = 125 },
+			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+		},
+	},
     lscustoms68 = {
 		label = 'LS Customs Route 68',
 		type = 'mechanic',
