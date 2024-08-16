8614f946c3dc60277605707d9fe6e6d753a01ea8
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-08-16 00:28:27 -0400

    Update vehicles.lua

--- shared/vehicles.lua
+++ shared/vehicles.lua
@@ -799,12 +799,13 @@ local Vehicles = {
     { model = '24model3',         name = '2024 Model 3',               brand = 'Tesla',           price = 26000,   category = 'donate',         type = 'automobile', shop = 'none' },
     { model = 'iyremakerolls',    name = 'Collinin Government',        brand = 'Rolls Royce',     price = 26000,   category = 'donate',         type = 'automobile', shop = 'none' },
     -- jakers-vehicles
+    { model = 'pvcjv',         name = 'Cruiser',                    brand = 'Vapid',           price = 26000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
     { model = 'pvajv',         name = 'Aleutian',                   brand = 'Vapid',           price = 52000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
     { model = 'pvsjv',         name = 'Scout',                      brand = 'Vapid',           price = 56000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
-    { model = 'sebbjv',        name = 'EMS STX',                    brand = 'Bravado',         price = 26000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
     { model = 'spbbjv',        name = 'STX',                        brand = 'Bravado',         price = 50000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
     { model = 'spvdjv',        name = 'Dominator',                  brand = 'Vapid',           price = 95000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' },
     { model = 'spvhjv',        name = 'Valkyrie',                   brand = 'Heli',            price = 100000,  category = 'emergency',      type = 'heli',       shop = 'none' },
+    { model = 'sebbjv',        name = 'EMS STX',                    brand = 'Bravado',         price = 26000,   category = 'emergency',      type = 'automobile', shop = 'mrpd' }, 
     { model = 'ambo1',         name = 'Ambulance',                  brand = 'Ambulance',       price = 26000,   category = 'emergency',      type = 'automobile', shop = 'none' },
 
 }
